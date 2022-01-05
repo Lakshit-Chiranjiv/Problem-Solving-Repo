1b60aa47e6df13812704b25a0cc3e65bbb12a7c0
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-01-05 21:18:09 +0530

    sum of infinite array

--- Codestudio DSA/arrays/InfiniteArraySum.java
+++ Codestudio DSA/arrays/InfiniteArraySum.java
@@ -3,6 +3,29 @@ package arrays;
 import java.util.*;
 public class InfiniteArraySum {
 
+    //accepted solution
+    public static List<Integer> sumInRanges2(int[] arr, int n, List<List<Long>> queries, int q) {
+        long M = 1000000007,L,R,a;
+        List<Integer> ans = new ArrayList<>();
+        ArrayList<Integer> pref_arr = new ArrayList<>();
+        pref_arr.add(0);
+        for (int i = 0; i < n; i++) {
+            pref_arr.add((int) ((arr[i]%M + pref_arr.get(i)%M)%M));
+        }
+        for (int i = 0;i<q;i++) {
+            L = queries.get(i).get(0);
+            R = queries.get(i).get(1);
+            L--;
+            a = (pref_arr.get((int) (R%n)) - pref_arr.get((int) (L%n)) + ((pref_arr.get(n) *((R/n - L/n)%M))))%M;
+            ans.add((int) ((a+M)%M));
+        }
+        return ans;
+    }
+
+    //correct for single cases but not accepted
+    //kind of ans is changing depending on the query number
+    //like if 1 5 query is at 3rd position it returns 77 instead of 55
+    //and if 1 5 query is at 4th position it returns 88 instead of 55
     public static List<Integer> sumInRanges(int[] arr, int n, List<List<Long>> queries, int q) {
 
         // Write your code here!
@@ -14,9 +37,9 @@ public class InfiniteArraySum {
             pref_arr.add((pref_arr.get(i-1) + arr[i])%M);
         }
 
-        for (int i = 0; i < n; i++) {
-            System.out.print(pref_arr.get(i)+",");
-        }
+//        for (int i = 0; i < n; i++) {
+//            System.out.print(pref_arr.get(i)+",");
+//        }
 
         List<Integer> ans = new ArrayList<>();
         for (int i = 0;i<q;i++)
@@ -53,12 +76,9 @@ public class InfiniteArraySum {
                             a += (arr[n-1] + (pref_arr.get((int) (R-1))))%M;
                     }
                     else {
-                        if(R==0){
-                            a += (pref_arr.get(n-1) - pref_arr.get((int) (L-2)))%M;
-                        }
-                        else {
-                            a += (pref_arr.get(n-1) - pref_arr.get((int) (L-2)))%M;
-                            a += (pref_arr.get((int) (R-1)))%M;
+                        a += (pref_arr.get(n-1) - pref_arr.get((int) (L-2)))%M;
+                        if (R != 0) {
+                            a += (pref_arr.get((int) (R - 1))) % M;
                         }
                     }
                 }
@@ -79,11 +99,15 @@ public class InfiniteArraySum {
 
         queries.add(qs);
         int q = 1;
-        List<Integer> ans;
-        ans = sumInRanges(arr,n,queries,q);
+        List<Integer> ans1,ans2;
+        ans1 = sumInRanges(arr,n,queries,q);
+        ans2 = sumInRanges2(arr,n,queries,q);
 
         for (int i = 0; i < q; i++) {
-            System.out.println("\nans="+ans.get(i));
+            System.out.println("\nans="+ans1.get(i));
+        }
+        for (int i = 0; i < q; i++) {
+            System.out.println("\nans="+ans2.get(i));
         }
     }
 }
