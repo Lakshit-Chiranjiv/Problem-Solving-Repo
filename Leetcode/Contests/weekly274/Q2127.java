4ce937741557e5854d57992770164177f387e352
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-01-08 20:05:30 +0530

    max invites- not yet done

---
+++ Leetcode/Contests/weekly274/Q2127.java
@@ -0,0 +1,30 @@
+package Contests.weekly274;
+//2127. Maximum Employees to Be Invited to a Meeting
+import java.util.*;
+public class Q2127 {
+    public static int maximumInvitations(int[] favorite) {
+        int[] participants = new int[favorite.length];
+        Arrays.fill(participants,0);
+        int maxInvites = favorite.length;
+        for (int i = 0; i < favorite.length; i++) {
+            if(i != favorite[favorite[i]])
+                participants[favorite[i]]++;
+        }
+
+        for (int participant : participants) {
+            if (participant > 1)
+                maxInvites--;
+        }
+
+        return maxInvites;
+    }
+
+    public static void main(String[] args) {
+        int[] favourite = {2,2,1,2};//-3 //1,2,0 - 3 //3,0,1,4,1 - 4
+        int[] favourite2 = {1,2,0};
+        int[] favourite3 = {3,0,1,4,1};
+        int[] favourite4 = {1,0,0,2,1,4,7,8,9,6,7,10,8};//6
+        int ans = maximumInvitations(favourite4);
+        System.out.println(ans);
+    }
+}
