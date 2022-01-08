9c97606236b4c9c4ab6fc3aa6da1386318b5d500
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-01-08 20:39:25 +0530

    max twin sum - done

---
+++ Leetcode/Contests/biweekly69/Q5961.java
@@ -0,0 +1,35 @@
+package Contests.biweekly69;
+//5961. Maximum Twin Sum of a Linked List
+
+import java.util.*;
+public class Q5961 {
+    ListNode ln = new ListNode();
+    public static int pairSum(ListNode head) {
+        long maxTwinSum = 0,currentTwinSum = 0;
+        ArrayList<Integer> arr = new ArrayList<>();
+        ListNode temp;
+        temp = head;
+        while (temp.next != null)
+        {
+            arr.add(temp.val);
+            temp = temp.next;
+        }
+        arr.add(temp.val);
+        int i = 0,j = arr.size()-1;
+        while (i<j){
+            currentTwinSum = arr.get(i) + arr.get(j);
+            maxTwinSum = Math.max(maxTwinSum,currentTwinSum);
+            i++;
+            j--;
+        }
+
+        return (int) maxTwinSum;
+
+    }
+
+    public static void main(String[] args) {
+        ListNode head;
+
+    }
+
+}
