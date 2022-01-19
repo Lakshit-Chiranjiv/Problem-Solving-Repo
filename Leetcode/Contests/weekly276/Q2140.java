f0176e2f8e9d5327e4335829f32ef72d5d34ab20
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-01-19 22:31:06 +0530

    solving ques with brainpower - complete

--- Leetcode/Contests/weekly276/Q2140.java
+++ Leetcode/Contests/weekly276/Q2140.java
@@ -1,8 +1,24 @@
 package Contests.weekly276;
 //2140. Solving Questions With Brainpower
-//correct but TLE
+//Points2 is correct but TLE
+//Points is accepted
 public class Q2140 {
+    static long[] dp;
     public static long Points(int idx,int[][] questions,int n) {
+
+        if(idx>=n) return 0;
+        if(dp[idx] > 0) return dp[idx];
+        int pts = questions[idx][0];
+        int pwr = questions[idx][1];
+
+        long pt1 = pts + Points(idx+pwr+1,questions,n);
+        long pt2 = Points(idx+1,questions,n);
+
+        dp[idx] = Math.max(pt1,pt2);
+        return dp[idx];
+    }
+
+    public static long Points2(int idx,int[][] questions,int n) {
 
         if(idx>=n) return 0;
         int pts = questions[idx][0];
@@ -14,7 +30,9 @@ public class Q2140 {
         return Math.max(pt1,pt2);
     }
 
+
     public static long mostPoints(int[][] questions) {
+        dp = new long[questions.length];
         return Points(0,questions,questions.length);
     }
 
