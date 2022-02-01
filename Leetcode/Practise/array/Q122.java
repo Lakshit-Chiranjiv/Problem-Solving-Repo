017689cc29b1c36ccaabb56008a79b9d9d0e511d
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-02-01 21:12:47 +0530

    Best Time to Buy and Sell Stock II - complete

---
+++ Leetcode/Practise/array/Q122.java
@@ -0,0 +1,20 @@
+package Practise.array;
+//122. Best Time to Buy and Sell Stock II
+public class Q122 {
+    public static int maxProfit(int[] prices) {
+        int p = 0;
+        for(int i =1;i<prices.length;i++){
+            if(prices[i-1]<prices[i])
+                p += (prices[i] - prices[i-1]);
+        }
+
+        return p;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+        int ans = maxProfit(prices);
+        System.out.println(ans);
+    }
+
+}
