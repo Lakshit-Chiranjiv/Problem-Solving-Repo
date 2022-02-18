e553f8fb1230543e0738760ca4b6b727d4da5469
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-02-18 20:56:04 +0530

    combination sum - complete

---
+++ Leetcode/Practise/array/Q39.java
@@ -0,0 +1,23 @@
+package Practise.array;
+//39. Combination Sum
+import java.util.*;
+public class Q39 {
+    List<List<Integer>> res = new ArrayList<>();
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        helper(candidates, target, 0, new ArrayList<Integer>());
+        return res;
+    }
+
+    public void helper(int[] cands, int target, int stIdx, List<Integer> curList) {
+        if (target == 0) res.add(curList);
+
+        for (int i = stIdx; i < cands.length; i++) {
+            if (cands[i] <= target) {
+                List<Integer> list = new ArrayList<>(curList);
+                list.add(cands[i]);
+                helper(cands, target - cands[i], i, list);
+            }
+        }
+    }
+}
